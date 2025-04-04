c945dd53329ad857a3821d6052e6e1606e689756
Rakshith272003 <rakshithmdpura@gmail.com>
2025-04-04 14:19:18 +0530

    message

---
+++ SeconProject/SeconProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeconProject
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Rakshith");
+        }
+    }
+}
---
+++ SeconProject/SeconProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SeconProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi from Program");
+        }
+    }
+}
+//this is done to run the different ".cs" file using the startup object
