5922932a08d13d0214297f42f58ff99228e12c33
Rakshith272003 <rakshithmdpura@gmail.com>
2025-04-07 12:07:10 +0530

    message

---
+++ C#checking/typechecking/Program.cs
@@ -0,0 +1,19 @@
+﻿class program{
+    static void Main(string[] args){
+        var a = "hello";
+        Console.WriteLine(a.GetType());
+        var b=1.000;
+        Console.WriteLine(b.GetType());
+        var c = new List<int>();
+        Console.WriteLine(c.GetType());
+        var d = new Dictionary<int,int>();
+        Console.WriteLine(d.GetType());
+        var e = new int[5];
+        Console.WriteLine(e.GetType());
+        var f = new int[5,5];
+        Console.WriteLine(f.GetType()); 
+        var g = new int[5,5,5];
+        Console.WriteLine(g.GetType());
+
+}
+}
