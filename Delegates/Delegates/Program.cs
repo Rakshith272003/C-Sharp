e94e17508b5b0e7745347abaff462511425671c5
Rakshith272003 <rakshithmdpura@gmail.com>
2025-04-28 12:54:01 +0530

    message

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿public delegate void ShowMessage(string message);
+
+public class DemoClass
+{
+    public static void Demo(string msg)
+    {
+        Console.WriteLine("Message Recived:"+msg);
+    }
+}
+
+class Program {
+    public static void Main(string[] args)
+    {
+        ShowMessage shw = new ShowMessage(DemoClass.Demo);
+        shw("hi message from delegates");
+    }
+}
+    
+
---
+++ MultiCastDelegates/MultiCastDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+public delegate void Notification (string message); 
+
+public class NotiificationsDemo
+{
+    public static void Email(string msg)
+    {
+        Console.WriteLine(msg + " Email Msg");
+    }
+    public static void SMS(string msg)
+    {
+        Console.WriteLine(msg + " SMS Message");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Notification notify = new Notification(NotiificationsDemo.Email);
+        notify += NotiificationsDemo.SMS;
+        notify("Hi");
+    }
+}
